5f2a019cbbde5e610922b853554e30d6c5146233
massi-brero <massi.brero@gmail.com>
2018-03-11 15:38:22 +0100

    changed intro color

--- frontend-console/src/main/java/de/mbrero/see/console/App.java
+++ frontend-console/src/main/java/de/mbrero/see/console/App.java
@@ -11,7 +11,7 @@ import de.mbrero.see.persistance.DBConnection;
  */
 public class App {
 	
-	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_GREEN = "\u001B[96m";
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_RED = "\u001B[31m";
 	
